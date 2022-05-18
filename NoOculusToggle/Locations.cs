ea59101f14ddced2391d8da17fa54773ea815002
luximus-hunter <thomaslipman10@gmail.com>
2022-05-19 01:26:28 +0200

    Add NoOculusToggle
    for admin stuff

---
+++ NoOculusToggle/Locations.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Win32;
+
+namespace NoOculusToggle
+{
+    internal static class Locations
+    {
+        public static string Root = @"C:\SteamVRHelper";
+        public static string TemplateDirectory = Path.Combine(Root, "TemplateFiles");
+
+        public static string BackupExtension = ".backup";
+
+        #region Steam
+
+        public static string Steam = Registry.GetValue(@"HKEY_CURRENT_USER\Software\Valve\Steam", "SteamPath", null).ToString();
+        public static string SteamGames = Path.Combine(Path.GetFullPath(Steam), @"steamapps\common\");
+
+        #endregion
+
+        #region Oculus
+
+        public static string OculusDirectory = @"C:\Program Files\Oculus\Support\oculus-dash\dash\bin";
+        public static string OculusFileName = "OculusDash.exe";
+        public static string OculusFile = Path.Combine(OculusDirectory, OculusFileName);
+        public static string OculusKillerFile = Path.Combine(TemplateDirectory, OculusFileName);
+        public static string OculusBackupFile = Path.Combine(OculusDirectory, OculusFileName + BackupExtension);
+
+        #endregion
+
+        #region OpenVR
+
+        public static string OpenvrDllFileName = "openvr_api.dll";
+        public static string OpenvrConfigFileName = "openvr_mod.cfg";
+        public static string OpenvrLogFileName = "openvr_mod.log";
+        public static string OpenvrDllFile = Path.Combine(TemplateDirectory, OpenvrDllFileName);
+        public static string OpenvrConfigFile = Path.Combine(TemplateDirectory, OpenvrConfigFileName);
+
+        #endregion
+
+        public static void CreateDirectory(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+    }
+}
---
+++ NoOculusToggle/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace NoOculusToggle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length < 1)
+            {
+                Console.WriteLine("No argument provided");
+            }
+            else if(args.Length > 1)
+            {
+                Console.WriteLine("Too many arguments provided");
+            }
+            else
+            {
+                switch(args[0])
+                {
+                    case "-b":
+                        Backup();
+                        break;
+                    case "-e":
+                        Enable();
+                        break;
+                    case "-d":
+                        Disable();
+                        break;
+                    default:
+                        Console.WriteLine("Argument not supported");
+                        break;
+                }
+            }
+        }
+
+        public static void Backup()
+        {
+            string activeFile = Locations.OculusFile;
+            string backupFile = Locations.OculusFile + Locations.BackupExtension;
+
+            if (!File.Exists(backupFile))
+            {
+                File.Copy(activeFile, backupFile, true);
+            }
+        }
+
+        public static void Enable()
+        {
+            File.Copy(Locations.OculusKillerFile, Locations.OculusFile, true);
+        }
+
+        public static void Disable()
+        {
+            File.Copy(Locations.OculusBackupFile, Locations.OculusFile, true);
+        }
+    }
+}
